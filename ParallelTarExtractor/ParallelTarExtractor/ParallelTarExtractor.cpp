3a02eee22f59af2da766227b0e01c9176b47c353
ed.roma19 <ed.roma19@gmail.com>
2017-02-20 17:19:37 -0500

    Working Parrallel Process
    Creates an extra process

--- ParallelTarExtractor/ParallelTarExtractor/ParallelTarExtractor.cpp
+++ ParallelTarExtractor/ParallelTarExtractor/ParallelTarExtractor.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <fstream>
 #include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
 
 using namespace std;
 
@@ -18,10 +20,32 @@ struct tar_header {
 	char pad[255];
 };
 
+bool nextHeader(ifstream& inputFile, int nextHeaderBlock)
+{
+	//check if end of file
+	if (inputFile.eof())
+		return false;
+	//Go to the next header
+	inputFile.seekg((nextHeaderBlock * 512));
+
+	//Check that the next header has content. If not go to the next block
+	while (inputFile.peek() == '\0' && inputFile.good())
+	{
+		inputFile.seekg(512, inputFile.cur);
+		if (!inputFile.good())
+			return false;
+	}
+
+	return true;
+}
+
 tar_header readHeader(ifstream& inputFile, int nextHeaderBlock)
 {
 	tar_header header;
-	inputFile.seekg( (nextHeaderBlock * 512));
+	//inputFile.seekg( (nextHeaderBlock * 512));
+	//while (inputFile.peek() == '/0')
+	//	inputFile.seekg(512, inputFile.cur);
+
 	inputFile.read((char *)&header, sizeof(header));
 
 	return header;
@@ -59,43 +83,65 @@ int main(int argc, char *argv[])
 		string tarFileName = argv[1];
 		inputFile.open(tarFileName);
 
+		inputFile.seekg(0, inputFile.end);
+		int lengthOfFile = inputFile.tellg();
+		inputFile.seekg(0, inputFile.beg);
+
 		if (inputFile.good())
 		{
-			do {
+			do
+			{
 				//read header
 				tar_header header = readHeader(inputFile, nextHeaderBlock);
-				//fork
+				nextHeaderBlock += ((511 + convertSizeToInt(header.size)) / 512) + 1; //an offset of 512 is the full header
 
+				//fork
+				pid_t pid = -1;
+				if (nextHeader(inputFile, nextHeaderBlock))
+				{
+					pid = fork();
+				}
+				//else
+				//	break;
+
+				if (pid == 0)
+				{
 					//if child
-					nextHeaderBlock += ((511 + convertSizeToInt(header.size)) / 512) + 1; //an offset of 512 is the full header
-					//continue;
-				
+					cout << "reading with child process" << endl;
+					//nextHeaderBlock += ((511 + convertSizeToInt(header.size)) / 512) + 1; //an offset of 512 is the full header
+					continue;
+				}
+
 				//if parent
 				int checksum = strtol(header.checksum, NULL, 8);
 
 				// Put spaces in the checksum area before we compute it.
 				for (int i = 0; i < 8; i++) {
-						header.checksum[i] = ' ';
+					header.checksum[i] = ' ';
 				}
+
 				//compute checksum
 				int computedChecksum = 0;
 				for (int i = 0; i < sizeof(header); i++) {
-					computedChecksum += ((char*) &header)[i];
+					computedChecksum += ((char*)&header)[i];
 				}
 				//write to file
 				if (computedChecksum == checksum)
 					writeBody(inputFile, header);
+				return EXIT_SUCCESS;
 
-			} while (!inputFile.eof()); //there are more blocks;
+			} while (nextHeader(inputFile, nextHeaderBlock)); //there are more headers
 
 			inputFile.close();
+			return EXIT_SUCCESS;
 		}
 		else
 			cout << "Error extracting file. Could not open source file" << endl;
 
 		return EXIT_SUCCESS;
 	}
-	cout << "Error extracting file. Must provide a file name" << endl;
+	else
+		cout << "Error extracting file. Must provide a file name" << endl;
 
 	return EXIT_SUCCESS;
 }
